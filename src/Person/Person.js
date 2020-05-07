bde2ed9765f39b9654deefe0c586e953689f2a0f
Garland Lai <garlandlai24@gmail.com>
2020-05-07 13:02:03 -0700

    update Person and css to use modules for the media qeury

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,29 +1,14 @@
 import React from "react";
-import "./Person.css";
-import styled from "styled-components";
-
-
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
+import classes from "./Person.css";
 
 const person = props => {
   const {name, age, click, changed } = props;
   return (
-    <StyledDiv>
+    <div className={classes.Person}>
     <p onClick={click}>I'm {name} and I am {age} years old!</p>
     <p onClick={click}>{props.children}</p>
     <input type="text" onChange={changed} value={props.name}/>
-  </StyledDiv>
+  </div>
   )
 }
 
