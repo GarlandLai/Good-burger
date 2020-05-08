79a970d1bbb511d6a087c68d066a4e96dd1ac10b
Garland Lai <garlandlai24@gmail.com>
2020-05-08 10:10:45 -0700

    create ErrorBoundary to handle custome error messages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // added options in css-loader in config (prod and dev) allow us to import classes below
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 class App extends Component {
   state = {
@@ -50,12 +51,14 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-            key={person.id}
-            click={() => this.deletePersonHandler(index)} 
-            name={person.name}
-            age={person.age} 
-            changed={(event) => this.nameChangedHandler(event, person.id)}/>
+            // key must be in outer element. Moved from person to errorboundary
+            return <ErrorBoundary key={person.id}>
+              <Person
+              click={() => this.deletePersonHandler(index)} 
+              name={person.name}
+              age={person.age} 
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+            </ErrorBoundary>
           })}
         </div>
       );
