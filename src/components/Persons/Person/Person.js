3b88883ea2e455c49deb72e03e6ba2c3a3dbe77a
Garland Lai <garlandlai24@gmail.com>
2020-05-16 12:17:19 -0700

    Change persons and person to class based components in order to practice component update lifecycle methods

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Person.css";
 
-const person = props => {
-  const {name, age, click, changed } = props;
-  console.log("person.js rendered")
-  return (
-    <div className={classes.Person}>
-    <p onClick={click}>I'm {name} and I am {age} years old!</p>
-    <p onClick={click}>{props.children}</p>
-    <input type="text" onChange={changed} value={props.name}/>
-  </div>
-  )
+class Person extends Component {
+  render() {
+    const {name, age, click, changed } = this.props;
+    console.log("person.js rendered")
+    return (
+      <div className={classes.Person}>
+      <p onClick={click}>I'm {name} and I am {age} years old!</p>
+      <p onClick={click}>{this.props.children}</p>
+      <input type="text" onChange={changed} value={name}/>
+    </div>
+    ) 
+  }
+
 }
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from "./Person/Person";
 
-const persons = (props) => {
-  console.log("persons.js rendered");
-  return props.persons.map((person, index) => {
-    return <Person
-      key={person.id}
-      click={() => props.clicked(index)} 
-      name={person.name}
-      age={person.age} 
-      changed={(event) => props.changed(event, person.id)}/>
-    }
-  );
+class Persons extends Component {
+  render() {
+    console.log("persons.js rendered");
+    return this.props.persons.map((person, index) => {
+      return <Person
+        key={person.id}
+        click={() => this.props.clicked(index)} 
+        name={person.name}
+        age={person.age} 
+        changed={(event) => this.props.changed(event, person.id)}/>
+      }
+    );
+  }
 }
-export default persons;
+
+export default Persons;
