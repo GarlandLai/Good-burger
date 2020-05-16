67d16814b4e7388bd9a9a91d383673326d46c36c
Garland Lai <garlandlai24@gmail.com>
2020-05-16 12:27:39 -0700

    use components update lifecycles methods

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import Person from "./Person/Person";
 
 class Persons extends Component {
+
+  // // Don't need this if we dont have state. It will fire though since its part of creation lifecycle
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log(" Persons getDerivedStateFromProps");
+  //   return state;
+  // }
+
+  shouldComponentUpdate() {
+    console.log("persons.js shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, nextState) {
+    console.log("persons.js getSnapshotBeforeUpdate");
+    // saves data before update that can be used in componentDidUpdate
+    return {message: "snapshot!"};
+  }
+
+  componentDidUpdate(prevProps,prevState, snapshot) {
+    console.log("persons.js componentDidUpdate")
+    console.log(snapshot);
+  }
+
   render() {
     console.log("persons.js rendered");
     return this.props.persons.map((person, index) => {
