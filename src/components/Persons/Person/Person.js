0125af69368800862b354a6a3ffd9ecf57b22547
Garland Lai <garlandlai24@gmail.com>
2020-05-20 10:49:49 -0700

    use adjacent jsx elements to person

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,13 +5,13 @@ class Person extends Component {
   render() {
     const {name, age, click, changed } = this.props;
     console.log("person.js rendered")
-    return (
-      <div className={classes.Person}>
-      <p onClick={click}>I'm {name} and I am {age} years old!</p>
-      <p onClick={click}>{this.props.children}</p>
-      <input type="text" onChange={changed} value={name}/>
-    </div>
-    ) 
+    return [
+      // <div className={classes.Person}>
+      <p key="p1"onClick={click}>I'm {name} and I am {age} years old!</p>,
+      <p key="p2"onClick={click}>{this.props.children}</p>,
+      <input key="p3" type="text" onChange={changed} value={name}/>
+    // </div>
+    ] 
   }
 
 }
