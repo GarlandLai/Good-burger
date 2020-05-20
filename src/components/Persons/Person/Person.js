0d109948c3798f3554eac80e5e65930eb9f8269f
Garland Lai <garlandlai24@gmail.com>
2020-05-20 10:55:52 -0700

    create hoc folder with aux component. Can be used as root element to return children.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import classes from "./Person.css";
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
   render() {
     const {name, age, click, changed } = this.props;
     console.log("person.js rendered")
-    return [
-      // <div className={classes.Person}>
-      <p key="p1"onClick={click}>I'm {name} and I am {age} years old!</p>,
-      <p key="p2"onClick={click}>{this.props.children}</p>,
-      <input key="p3" type="text" onChange={changed} value={name}/>
-    // </div>
-    ] 
+    return (
+      <Aux>
+      {/* <div className={classes.Person}> */}
+      <p onClick={click}>I'm {name} and I am {age} years old!</p>
+      <p onClick={click}>{this.props.children}</p>
+      <input type="text" onChange={changed} value={name}/>
+      {/* </div> */}
+      </Aux>
+    ) 
   }
 
 }
